05d9eaa3e8db196f962f4df9859c561d931797da
Raj Goud <goudraj125@gmail.com>
2026-01-14 19:10:29 +0530

    update footer

--- src/component/student/Footer.jsx
+++ src/component/student/Footer.jsx
@@ -10,14 +10,22 @@ return (
             </div>
             <div className='flex flex-col md:items-start items-center w-full'>
                 <h2 className='font-semibold text-white mb-5'>Company</h2>
-                <ul>
+                <ul className='flex md:flex-col w-full justify-between text-sm text-white/80 md:space-y-2'>
                     <li><a href="#">Home</a></li>
-                    <li><a href="#">About us</a></li>∟
-                    <li><a href="#">Contact us</a></li>∟
-                    <li><a href="#">Privacy policy</a></li>∟▬
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Contact us</a></li>
+                    <li><a href="#">Privacy policy</a></li>
                 </ul>
             </div>
-            <div></div>
+            <div className='hidden md:flex flex-col items-start w-full'>
+                <h2 className='font-semibold text-white mb-5'>Subscribe to our newsletter</h2>
+                <p className='text-sm text-white/80'>The latest news, articles, and resources, sent to your inbox weekly.</p>
+                <div className='flex items-center gap-2 pt-4'>
+                    <input type="email" placeholder='Enter your email' className='border border-gray-500/30 bg-gray-800 text-gray-500
+                    placeholder-gray-500 outline-none w-64 h-9 rounded  px-2 text-sm'/>
+                    <button className='bg-blue-600 w-24 h-9 rounded text-white'>Susbcribe</button>
+                </div>
+            </div>
         </div> 
         <p className='py-4 text-center text-xs md:text-sm text-white/60'>Copyright 2024 © Edemy. All Right Reserved.</p>
   </footer>
